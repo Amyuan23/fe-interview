949059dda6f0210ee5a3f35c8133d20b52aa3078
yuan <amyuan23@gmail.com>
2022-05-03 17:35:50 +0800

    feat: 对象数组去重

--- utils/array.ts
+++ utils/array.ts
@@ -1,17 +1,32 @@
 /**
  * 对象数组去重
- * @param arr 需要去重对象数组
- * @param key 去重根据，取对象的一个key
+ * @param arr 需要去重对象数组或者数组
+ * @param key 去重根据，取对象的一个key,数组的时候不用传
  * @returns
  */
-export const unique = (arr, key) => {
+const unique = (arr, key = '') => {
   if (!arr) return arr
-  if (key === undefined) return [...new Set(arr)]
-  const map = {
-    string: (e) => e[key],
-    function: (e) => key(e)
-  }
-  const fn = map[typeof key]
-  const obj = arr.reduce((o, e) => ((o[fn(e)] = e), o), {})
+  if (!key) return [...new Set(arr)]
+
+  const fn = (obj) => obj[key]
+  const obj = arr.reduce((prev, cur) => {
+    prev[fn(cur)] = cur
+    return prev
+  }, {})
   return Object.values(obj)
 }
+
+// 测试
+// const list = [
+//   {
+//     name: '1'
+//   },
+//   {
+//     name: '2'
+//   },
+//   {
+//     name: '2'
+//   }
+// ]
+
+// console.log(unique(list, 'name'))
