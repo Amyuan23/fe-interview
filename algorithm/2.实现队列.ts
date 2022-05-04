31488e801eb0ac611da858b2970b7da8d8c91e97
yuan <amyuan23@gmail.com>
2022-05-04 16:45:22 +0800

    feat: 队列

---
+++ algorithm/2.实现队列.ts
@@ -0,0 +1,54 @@
+class Queue {
+  items
+  constructor() {
+    this.items = []
+  }
+
+  // 加入队列
+  enqueue(item) {
+    this.items.push(item)
+  }
+
+  // 出队列
+  dequeue() {
+    return this.items.shift()
+  }
+
+  // 查看前端的元素
+  front() {
+    return this.items[0]
+  }
+
+  isEmpty() {
+    return this.items.length === 0
+  }
+
+  size() {
+    return this.items.length
+  }
+
+  toString() {
+    return this.items.join(' ')
+  }
+}
+
+// 击鼓传花
+function passGame(nameList, num) {
+  const queue = new Queue()
+  // 加入队列
+  for (let index = 0; index < nameList.length; index++) {
+    queue.enqueue(nameList[index])
+  }
+
+  while (queue.size() > 1) {
+    for (let index = 0; index < num; index++) {
+      const item = queue.dequeue()
+      queue.enqueue(item)
+    }
+    queue.dequeue()
+  }
+
+  console.log('最后剩下的人：' + queue.front())
+}
+
+passGame(['kk', '刷', '小将'], 3)
