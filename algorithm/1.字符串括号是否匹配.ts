205b408c28971fb0bcd365cdba531a3ad063104f
yuan <amyuan23@gmail.com>
2022-05-05 23:23:25 +0800

    feat: 算法优化

--- algorithm/1.字符串括号是否匹配.ts
+++ algorithm/1.字符串括号是否匹配.ts
@@ -1,25 +1,28 @@
 // 思路：压栈，出栈
-// {} []
+// {} [] ()
 function matchBracket(str) {
   const length = str.length
-  const stackList = []
+  if (length === 0) return true
+
+  const stack = []
+  const leftSymbols = '{[('
+  const rightSymbols = '}])'
   for (let index = 0; index < length; index++) {
     const cur = str[index]
-    if ('{}[]'.indexOf(cur) < 0) continue
-    if (!stackList.length) {
-      stackList.push(str[index])
-    } else {
-      const start = stackList[stackList.length - 1]
-      if ((start === '{' && cur === '}') || (start === '[' && cur === ']')) {
-        stackList.pop()
+    if (leftSymbols.includes(cur)) {
+      stack.push(cur)
+    } else if (rightSymbols.includes(cur)) {
+      const top = stack[stack.length - 1]
+      if ((top === '{' && cur === '}') || (top === '[' && cur === ']') || (top === '(' && cur === ')')) {
+        stack.pop()
       } else {
-        stackList.push(cur)
+        return false
       }
     }
   }
-  return !stackList.length
+  return !stack.length
 }
 
 // 测试
-console.log(matchBracket('{a[999]bbb)}'))
+console.log(matchBracket('{a[999]bbb}'))
 console.log(matchBracket('{a[999}bbb]'))
