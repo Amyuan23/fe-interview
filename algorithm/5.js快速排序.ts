1ea6d0f8d5c0e5e2e0d845b54a89f724fbc823d7
yuan <amyuan23@gmail.com>
2022-05-08 16:27:17 +0800

    feat: 排序

---
+++ algorithm/5.js快速排序.ts
@@ -0,0 +1,29 @@
+/**
+ * 快速排序
+ * @param arr [1,3,7,5,2,8,9,5]
+ * @returns []
+ */
+const quickSort = (arr: number[]): number[] => {
+  const length = arr.length
+  if (length === 0) return []
+
+  const midIndex = Math.floor(length / 2)
+  const midValue = arr[midIndex]
+
+  const left = []
+  const right = []
+
+  for (let i = 0; i < length; i++) {
+    if (midIndex !== i) {
+      if (arr[i] <= midValue) {
+        left.push(arr[i])
+      } else {
+        right.push(arr[i])
+      }
+    }
+  }
+
+  return quickSort(left).concat([midValue], quickSort(right))
+}
+
+console.log(quickSort([1, 3, 7, 5, 2, 8, 9, 5]))
