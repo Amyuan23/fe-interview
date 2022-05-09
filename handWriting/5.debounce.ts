5e282005608da93eed4180d231df2f1774054b3d
yuan <amyuan23@gmail.com>
2022-05-09 23:51:35 +0800

    feat: 防抖

---
+++ handWriting/5.debounce.ts
@@ -0,0 +1,15 @@
+function debounce(fn, wait: number) {
+  let timer = 0
+  return function () {
+    timer && clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+      timer = 0
+    }, wait)
+  }
+}
+
+// 使用
+document.getElementsByTagName('body')[0].onmousemove = debounce(() => {
+  console.log('kkkkk')
+}, 1000)
