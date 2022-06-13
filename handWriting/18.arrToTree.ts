2be9a4125fa575a4be519bd01fe97c0e54300333
yuan <amyuan23@gmail.com>
2022-06-13 10:24:35 +0800

    feat: 数组转树

--- handWriting/18.arrToTree.ts
+++ handWriting/18.arrToTree.ts
@@ -1,1 +1,30 @@
- 
+const arr2 = [
+  { id: 1, name: '部门1', pid: 0 },
+  { id: 2, name: '部门2', pid: 1 },
+  { id: 3, name: '部门3', pid: 1 },
+  { id: 4, name: '部门4', pid: 3 },
+  { id: 5, name: '部门5', pid: 4 }
+]
+
+type Arr = typeof arr2
+
+// 数组转树
+
+const arrToTree = (arr: Arr) => {
+  const treeMap = new Map()
+
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index]
+    treeMap.set(element.id, { ...element })
+
+    if (treeMap.get(element.pid)) {
+      // 有父节点
+      const parentNode = treeMap.get(element.pid)
+      if (!parentNode.children) parentNode.children = []
+      parentNode.children.push({ ...element })
+    }
+  }
+
+  // 返回根节点
+  return treeMap.get(0)
+}
